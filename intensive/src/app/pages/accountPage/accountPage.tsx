285de33a4c56f79b9d48dddf40c463986359dc24
Pavel <nightcharm@mail.ru>
2023-11-16 09:02:50 +0300

    add link on accoun page

--- intensive/src/app/pages/accountPage/accountPage.tsx
+++ intensive/src/app/pages/accountPage/accountPage.tsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+import styles from"./AccountPage.module.scss"
 const AccounPage = () => {
+
     return ( 
+        <>
         <h1>Личный кабинет</h1>
+        <Link to={"/"} className={styles.account__link}> На главную</Link>
+        </>
      );
 }
  
